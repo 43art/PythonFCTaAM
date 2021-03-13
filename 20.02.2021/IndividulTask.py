9792e52ab32529852470726d74aa1176ab12d7b9
Alexey Zuev <alconvert@mail.ru>
2021-03-13 10:11:24 +0300

    "Completed 3-d IndividulTask.py"

--- 20.02.2021/IndividulTask.py
+++ 20.02.2021/IndividulTask.py
@@ -7,22 +7,63 @@
 file_input = open("input.txt", "r")
 file_output = open("output.txt", "w")
 
-dictionary = {}
+dictionaryIP = {}
+dictionaryDays = {'Monday': 0, 'Tuesday': 0, 'Wednesday': 0, 'Thursday': 0, 'Friday': 0, 'Saturday': 0, 'Sunday': 0}
+dictionaryTime = {}
 
 with file_input:
-    firstline = file_input.readline()
-arrayLine = firstline.split()
-dictionary.update({arrayLine[0]: 1})
+    firstLine = file_input.readline()
+    listLine = firstLine.split()
+    dictionaryIP.update({listLine[0]: 1})
+    listTime = str(listLine[1]).split(':')
+    dictionaryTime.update({listTime[0]: 1})
+    for j in dictionaryDays.keys():
+        if j == listLine[2]:
+            count = dictionaryDays[j] + 1
+            dictionaryDays.update({listLine[2]: count})
+            break
+    listLine.clear()
+    listTime.clear()
+    for line in file_input:
+        listLine = line.split()
+        flag = 0
+        for j in dictionaryIP.keys():
+            if j == listLine[0]:
+                count = dictionaryIP[j] + 1
+                dictionaryIP.update({listLine[0]: count})
+                flag = 1
+                break
+        if flag == 0:
+            dictionaryIP.update({listLine[0]: 1})
+        listTime = str(listLine[1]).split(':')
+        flag = 0
+        for j in dictionaryTime.keys():
+            if j == listTime[0]:
+                count = dictionaryTime[j] + 1
+                dictionaryTime.update({listTime[0]: count})
+                flag = 1
+                break
+        if flag == 0:
+            dictionaryTime.update({listTime[0]: 1})
+        for j in dictionaryDays.keys():
+            if j == listLine[2]:
+                count = dictionaryDays[j] + 1
+                dictionaryDays.update({listLine[2]: count})
+                flag = 1
+                break
+        listLine.clear()
+        listTime.clear()
 
-file_input = open("input.txt", "r")
-with file_input:
-    for line in range(1):
-        file_input.readline()
-        arrayLines = file_input.readline()
+with file_output:
+    for key, val in dictionaryIP.items():
+        file_output.write(str(key) + ' visited the site ' + str(val) + ' times\n')
+    for key, val in dictionaryDays.items():
+        file_output.write('On ' + str(key) + ' the number of visits ' + str(val) + '\n')
+    popularTimeCount = 0
+    for key, val in dictionaryTime.items():
+        if popularTimeCount < val:
+            popularTime = key
+            popularTimeCount = val
 
-print(arrayLines)
-
-
-#dictionary.update({surname: vote})
-
-#print(text)
+    file_output.write('Most popular visiting time (hour) ' + str(popularTime) +
+                      ' at this time the number of visits was ' + str(popularTimeCount) + ' times\n')
